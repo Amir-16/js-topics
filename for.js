d489dbe14a0c730fd9752486aabfd3f5d3c30a66
Your Name <greenfitter516@gmail.com>
2021-04-14 23:39:13 +0600

    js start

---
+++ for.js
@@ -0,0 +1,14 @@
+var age =[10,12,23,24,25,24,28];
+
+for(var i=0;i<age.length;i++){
+    var persons =age[i];
+    //console.log(persons);
+}
+
+var name =['abir','rahim','karim'];
+
+for(var i=0;i<name.length;i++){
+    
+    var total =name[i];
+    console.log(total);
+}
---
+++ while.js
@@ -0,0 +1,6 @@
+var no = 5;
+
+while(no<=15){
+    console.log(no);
+    no++;
+}

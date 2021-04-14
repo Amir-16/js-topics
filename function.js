c7a976eb03ef9cb2f4e16ebff4180933d002bc7d
Your Name <greenfitter516@gmail.com>
2021-04-15 03:30:53 +0600

    js add

--- function.js
+++ function.js
@@ -6,3 +6,13 @@ function add(no1,no2){
 
 var ans =add(5,7);
 console.log(ans);
+
+
+function multiple(value1,value2){
+
+    var total =value1 *value2;
+    return total;
+}
+
+var result =multiple(15,1110);
+console.log(result);
---
+++ object.js
@@ -0,0 +1,11 @@
+var info ={id:1010, address:"ctg", designation:"Engineer" };
+
+//console.log(info);
+
+
+var property =info.address;
+
+
+//change the property
+info["designation"] ="Software Engineer";
+console.log(info);

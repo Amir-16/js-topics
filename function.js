5ddf90c19a3314367426e090533bba3e46a7145b
Your Name <greenfitter516@gmail.com>
2021-04-15 02:11:24 +0600

    js add

---
+++ function.js
@@ -0,0 +1,8 @@
+function add(no1,no2){
+
+    var sum =no1+no2;
+    return sum;
+}
+
+var ans =add(5,7);
+console.log(ans);
---
+++ switch.js
@@ -0,0 +1,26 @@
+// switch(condition){
+
+//case 
+//expression
+//va
+}
+
+
+
+no =100;
+
+switch(no){
+    case  20:
+    console.log("print case1")
+    break;
+    case 50:
+    console.log("print case2")
+    break;
+    case 40:
+    console.log("print case3")
+    break;
+    default :
+    console.log("print default value");
+    break;
+
+}
